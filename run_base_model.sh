588f98cdf403d308ceaf123caa2a1163da36c935
Laurie <laurence.ho@durham.ac.uk>
2021-04-08 20:25:07 +0100

    split up run scripts

---
+++ run_base_model.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -c 1
+#SBATCH --gres=gpu
+#SBATCH -p ug-gpu-small
+#SBATCH --qos=short
+#SBATCH -t 07-00:00:00
+#SBATCH --job-name=sketchr2pix2pixtests
+#SBATCH --mem=24G
+#SBATCH --mail-user laurence.ho@durham.ac.uk
+#SBATCH --mail-type=ALL
+
+source ~/pix2pixenv/bin/activate
+module load cuda/10.1-cudnn7.6
+
+python3 sketch-r2pix2pix_train.py --discrim_layers 3 --dataroot ./datasets/sketchy --name sr2pix2pix_core_3_layers --model sketchr2pix2pix --direction AtoB --display_id=0 --input_nc 8 --output_nc 3 --save_epoch_freq 2
